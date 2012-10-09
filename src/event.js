757de861ac4db22e4a22b9b54e48def8f337d9b3
赵 书香 <ZhaoShuxiang@zhaomatoMacBook-Pro.local>
2012-10-09 10:35:00 +0800

    创建事件对象
    事件对象用于混淆或继承于其他对象，使其他对象拥有事件的绑定，解除绑定，触发功能，方便对象只见的消息传递，实现解藕。

---
+++ src/event.js
@@ -0,0 +1,43 @@
+var k_event = {};
+
+k_event.on = function (eventName, callback) {
+    var callbacks = this.callbacks = this.callbacks || {};
+     
+    if ('string' === typeof eventName && 'function' === typeof callback) {
+        (callbacks[eventName] || callbacks[eventName] = []).push(callback);
+    }
+
+    return this;
+};
+
+k_event.trigger = function () {
+    var i = 0, len, callbackArray;
+    var args = Array.prototype.slice.call(arguments, 0);
+    var eventName = args.shift();
+
+    if ('string' === typeof eventName && this.callbacks &&
+            this.callbacks[eventName]) {
+
+        callbackArray = this.callbacks[eventName];
+        len = callbackArray.length;
+
+        for (; i< len; i ++) {
+            callbackArray[i].apply(this, args);
+        }
+    }
+
+    return this;
+};
+
+k_event.off = function () {
+    var eventName = arguments[0];
+
+    if (eventName && this.callbacks[eventName]) {
+        this.callbacks[eventName] = undefined;
+    }
+
+    return this;
+};
+
+
+
